4a409436096b1f6c70156b19150092edd719841e
RachaelChar <charewicz.r@gmail.com>
2020-01-02 18:25:46 -0500

    create db & build table

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,11 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
+    -- id: an auto incrementing int that serves as the primary key.
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+    -- burger_name: a string.
+	burger_name varchar(255) NOT NULL,
+    -- devoured: a boolean.
+	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
