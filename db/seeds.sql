0f4b7dbb3977dc90cc360fd0f8c4d0d0b41983ab
RachaelChar <charewicz.r@gmail.com>
2020-01-03 17:18:51 -0500

    insert into db

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- write insert queries to populate the burgers table with at least three entries.
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+    ("Mushroom & Swiss", "FALSE"),
+    ("Avocado Turkey", "FASLE"),
+    ("Veggie", "FALSE");
+
+
