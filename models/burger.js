de06192eee8ab17a98290fbb8d3c627d6d6447a5
RachaelChar <charewicz.r@gmail.com>
2020-01-05 10:30:15 -0500

    add burger object with methods

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,28 @@
 const orm = require("../config/orm.js");
+
+const burger = {
+    selectAll: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(colVals, condition, cb) {
+      orm.update("burgers", colVals, condition, function(res) {
+        cb(res);
+      });
+    },
+    delete: function(condition, cb) {
+      orm.delete("burgers", condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller (burgers_controller.js).
+  module.exports = burger;
