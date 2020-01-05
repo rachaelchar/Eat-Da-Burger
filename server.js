87ce8db90413096a5c0e5e6ab0fd66087b4f1ee2
RachaelChar <charewicz.r@gmail.com>
2020-01-05 13:35:59 -0500

    add routes, require dotenv, serve static files

--- server.js
+++ server.js
@@ -4,11 +4,30 @@ const exphbs = require("express-handlebars");
 
 // Create an instance of the express app.
 const app = express();
+require("dotenv").config();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 const PORT = process.env.PORT || 8080;
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
 // Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
