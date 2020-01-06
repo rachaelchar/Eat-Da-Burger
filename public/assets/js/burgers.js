ef80fc9b998a24c352555c1605de520d4a173d22
RachaelChar <charewicz.r@gmail.com>
2020-01-06 18:10:35 -0500

    add devour functionality

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,8 +1,7 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-
+    // New burger submission
     $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
       var newBurger = {
@@ -10,18 +9,44 @@ $(function() {
         devoured: false
       };
   
-      // Send the POST request.
       $.ajax("/api/burgers", {
         type: "POST",
         data: newBurger
       }).then(
         function() {
           console.log("created new burger");
-          // Reload the page to get the updated list
           location.reload();
         }
       );
     });
+    
+    // Update/devour burger
+    $(".devour-it").on("click", function(event) {
+
+
+        var id = $(this).data("burgerid");
+        var newState = $(this).data("devoured");
+        console.log("this", $(this).data("burgerid"));
+
+
+        const newDevouredState = {
+          devoured: true
+        };
+
+
+    
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newDevouredState
+        }).then(
+          function() {
+            console.log("changed burger to", newDevouredState);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
   
   });
   
